71c9da63bdc5421b309acc98f4e98712d6507c4e
brook7g <birukefekadu632@gmail.com>
2024-04-19 11:33:41 +0300

    Add billing functionality to timer

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+let bill = document.getElementById("bill");
 
+let startTime = new Date();
 setInterval(() => {
   let currentTime = new Date();
   hrs.innerHTML =
-    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    (currentTime.getHours() < 0 ? "0" : "") + currentTime.getHours();
   min.innerHTML =
-    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    (currentTime.getMinutes() < 0 ? "0" : "") + currentTime.getMinutes();
   sec.innerHTML =
-    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+    (currentTime.getSeconds() < 0 ? "0" : "") + currentTime.getSeconds();
+
+  let elapsedTimeInMinutes = (currentTime - startTime) / (1000 * 60);
+
+  let ratePerMinute = 5;
+
+  let totalBill = elapsedTimeInMinutes * ratePerMinute;
+
+  bill.innerHTML = `$${totalBill.toFixed(2)}`;
 }, 1000);
+
+//
